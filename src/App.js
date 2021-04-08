82951df65f916e4a47672b33af53db2cb84a53a4
Roman <vzdrizhni@gmail.com>
2021-04-08 13:22:47 +0500

    Add SignUp form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
-import './App.css';
+import React from 'react'
+import Navbar from './pages/navbar/navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar />
+    </div>
+  );
+}
 
 export default App;
---
+++ src/components/SignUp/signUp.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <form>
+                <h3>Sign Up</h3>
+
+                <div className="form-group">
+                    <label>Name</label>
+                    <input type="text" className="form-control" placeholder="First name" />
+                </div>
+
+                <div className="form-group">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                <p className="forgot-password text-right">
+                    Already registered <a href="#">sign in?</a>
+                </p>
+            </form>
+        );
+    }
+}
