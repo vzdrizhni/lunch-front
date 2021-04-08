51745f4b546399c36cff416e0ff736df62e0ccae
Roman <vzdrizhni@gmail.com>
2021-04-08 14:50:30 +0500

    Register user

--- src/components/SignUp/signUp.js
+++ src/components/SignUp/signUp.js
@@ -1,24 +1,52 @@
 import React, { Component } from "react";
 
 export default class SignUp extends Component {
+    state = {
+        username: '',
+        email: '',
+        password: ''
+    }
+
+    register = (e) => {
+        e.preventDefault()
+
+        fetch('http://localhost:3000/users',{
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state)
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    }
+
+    handleChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+        console.log(this.state);
+    };
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.register}>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>Name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" className="form-control" placeholder="First name" name="username" onChange={this.handleChange} />
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" placeholder="Enter email" name="email" onChange={this.handleChange} />
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" placeholder="Enter password" name="password" onChange={this.handleChange}/>
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
