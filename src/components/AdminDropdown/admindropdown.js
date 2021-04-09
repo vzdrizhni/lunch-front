b0090f234b7e032f830fe8dacef9a66c7c7df116
Roman <vzdrizhni@gmail.com>
2021-04-10 00:06:55 +0500

    Add menu item creation

--- src/components/AdminDropdown/admindropdown.js
+++ src/components/AdminDropdown/admindropdown.js
@@ -3,12 +3,17 @@ import React from 'react'
 import { DropdownButton, Dropdown } from 'react-bootstrap'
 import { BrowserRouter , Switch, Route, Link, Redirect } from "react-router-dom";
 
+import CreateMenuItem from '../CreateMenuItem/createMenuItem';
+
 const Admindropdown = () => {
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
         <DropdownButton id="dropdown-basic-button" title="Admin button">
             <Link to='/createWeekDay'><Dropdown.Item as="button">Create a weekday</Dropdown.Item></Link>
-            <Link to='/createMealItem'><Dropdown.Item as="button">Create meal item</Dropdown.Item></Link>
+            <Dropdown.Item as="button" onClick={() => setModalShow(true)}>Create meal item</Dropdown.Item>
             <Link to='updateWeekDay'><Dropdown.Item as="button">Something else</Dropdown.Item></Link>
+            <CreateMenuItem show={modalShow} onHide={() => setModalShow(false)} />
         </DropdownButton>
     )
 }
---
+++ src/components/CreateMenuItem/createMenuItem.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {useState} from 'react';
+
+import {Modal, Button, Form} from 'react-bootstrap';
+import {connect} from 'react-redux';
+
+const CreateMenuItem = (props) => {
+
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [menu_item_type, setMenuItemType] = useState('');
+    const [image, setImage] = useState('');
+
+    console.log(name, price, menu_item_type, image);
+
+
+    const createItemHandler = (e) => {
+        e.preventDefault()
+
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('price', price);
+        formData.append('menu_item_type', menu_item_type.toLowerCase());
+        formData.append('image', image);
+
+        fetch('http://localhost:3000/menu_items', {
+            method: 'POST',
+            headers: {
+                Authorization: 'Bearer ' + props.user.token,
+            },
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+        })
+        .catch(err => console.log(err));
+    }
+
+    return (
+        <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Create A Meal Item
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form onSubmit={createItemHandler}>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter Name" onChange={(e) => setName(e.target.value)} />
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Price</Form.Label>
+              <Form.Control type="text" placeholder="Enter Price" onChange={(e) => setPrice(e.target.value)}/>
+              <Form.Text className="text-muted"></Form.Text>
+            </Form.Group>
+            <Form.Label>Choose Menu Item Type</Form.Label>
+            <Form.Control as="select" onChange={(e) => setMenuItemType(e.target.value)}>
+                <option>First_Course</option>
+                <option>Main_Course</option>
+                <option>Drink</option>
+            </Form.Control>
+            <Form.Group>
+                <Form.File id="exampleFormControlFile1" label="Choose a picture" onChange={(e) => setImage(e.target.files[0])} />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={props.onHide}>
+              Submit
+            </Button>
+        </Form>
+        </Modal.Body>
+      </Modal>
+    )
+}
+
+const mapStateToProps = state => ({ user: state.user.currentUser });
+
+export default connect(mapStateToProps)(CreateMenuItem);
