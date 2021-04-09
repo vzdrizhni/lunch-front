658903d9587a2be29348db8f806ff84caf703475
Roman <vzdrizhni@gmail.com>
2021-04-09 19:29:42 +0500

    Create and redirect to root after creation

--- src/pages/CreateWeekDay/createWeekDay.js
+++ src/pages/CreateWeekDay/createWeekDay.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
 import {Button} from 'react-bootstrap';
+import {connect} from 'react-redux';
 
 import '../Weekdays/weekdays.css'
 
-const CreateWeekDay = () => {
+const CreateWeekDay = (props) => {
+
+    console.log(props);
+
+    const createDay = () => {
+        fetch('http://localhost:3000/weekdays', {
+            method: 'POST',
+            headers: {
+                Authorization: 'Bearer ' + props.user.token,
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response => response.json())
+        .then(() => {
+            props.history.push('/');
+        })
+        .catch(err => console.log(err))
+    }
+
+
     return (
         <div className='weekdays'>
             <div className="mb-2">
-                <Button variant="light" size="lg">
+                <Button variant="light" size="lg" onClick={createDay} >
                  Create A WeekDay For Today
                 </Button>
             </div>
@@ -16,4 +36,6 @@ const CreateWeekDay = () => {
     )
 }
 
-export default CreateWeekDay;
+const mapStateToProps = state => ({ user: state.user.currentUser });
+
+export default connect(mapStateToProps)(CreateWeekDay);
