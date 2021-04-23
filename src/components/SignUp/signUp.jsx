4571d79e35947006778bb3b3fc9a149214aa1be9
Roman <vzdrizhni@gmail.com>
2021-04-23 19:19:18 +0500

    Add checkbox to create admin if user wants it

---
+++ src/components/SignUp/signUp.jsx
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+import "./signUp.css";
+
+import { connect } from "react-redux";
+import { setUser } from "../../redux/user/user.actions";
+
+class SignUp extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    warning: "",
+    admin: false
+  };
+
+  register = (e) => {
+    e.preventDefault();
+
+    fetch("https://frozen-spire-70160.herokuapp.com/users", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(this.state),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          this.setState({ warning: data.error });
+        } else {
+          this.props.setUser(data);
+          this.setState({
+            username: "",
+            email: "",
+            password: "",
+            warning: "Account was created",
+          });
+          setTimeout(() => {
+            this.setState({ warning: "" });
+            this.props.history.push("/weekdays");
+          }, 2000);
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <form onSubmit={this.register}>
+            <h3>Sign Up</h3>
+
+            <div>
+              <span className="warning">{this.state.warning}</span>
+            </div>
+
+            <div className="form-group">
+              <label>Name</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="First name"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <div className="custom-control custom-checkbox">
+                <input
+                  type="checkbox"
+                  className="custom-control-input"
+                  id="customCheck1"
+                  onChange={() => this.setState({admin: !this.state.admin})}
+                />
+                <label className="custom-control-label" htmlFor="customCheck1">
+                  Want to be an admin?
+                </label>
+              </div>
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-block">
+              Sign Up
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({ user: state.user.currentUser });
+
+const mapDispatchToProps = (dispatch) => ({
+  setUser: (value) => dispatch(setUser(value)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
