4361b41307e4891d2a47891ef1bc258018744ff4
Roman <vzdrizhni@gmail.com>
2021-04-26 15:33:48 +0500

    Add status message on change

--- src/components/OrdersForTheCurrentDay/currentDayOrder.js
+++ src/components/OrdersForTheCurrentDay/currentDayOrder.js
@@ -1,54 +1,86 @@
-import { Card, ListGroup, Button, Dropdown, DropdownButton } from 'react-bootstrap';
+import {
+  Card,
+  ListGroup,
+  Button,
+  Dropdown,
+  DropdownButton,
+} from "react-bootstrap";
 
-import {useState} from 'react'
-import {connect} from 'react-redux';
+import { useState } from "react";
+import { connect } from "react-redux";
 
-import {setTrigger} from '../../redux/trigger/trigger.actions';
+import { setTrigger } from "../../redux/trigger/trigger.actions";
 
-import './orderCard.css'
+import "./orderCard.css";
 
 const CurrentDayOrders = (props) => {
+  const [status, setStatus] = useState(props.status);
+  const [statusDisplay, setStatusDisplay] = useState("none");
 
-    const [status, setStatus] = useState(props.status);
+  const statusChanger = () => {
+    fetch("https://frozen-spire-70160.herokuapp.com/orders/" + props.id, {
+      method: "PATCH",
+      headers: {
+        Authorization: "Bearer " + props.user.token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ status: status }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        props.setTrigger();
+        setStatusDisplay('inline-block')
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const statusChanger = () => {
-        fetch('https://frozen-spire-70160.herokuapp.com/orders/'+props.id, {
-            method: 'PATCH',
-            headers: {
-                Authorization: 'Bearer ' + props.user.token,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({status: status})
-        })
-        .then(response => response.json())
-        .then(data => props.setTrigger())
-        .catch(err => console.log(err))
-    }
-
-    return(
-        <Card style={{ width: '18rem' }} className="order-card">
-            <Card.Header><span>Date: {props.name}</span></Card.Header>
-            <ListGroup variant="flush" >
-                <ListGroup.Item className="status"><span>Status:</span>
-                <Dropdown>
-                    <DropdownButton id="dropdown-basic-button" title={status} onSelect={(e) => setStatus(e)}>
-                        <Dropdown.Item eventKey="accepted">Accept</Dropdown.Item>
-                        <Dropdown.Item eventKey="rejected">Reject</Dropdown.Item>
-                        <Dropdown.Item eventKey="pending">Pending</Dropdown.Item>
-                    </DropdownButton>
-                </Dropdown>
-              </ListGroup.Item>
-              <ListGroup.Item><span>Price: {props.total_price} $</span></ListGroup.Item>
-            </ListGroup>
-            <Button variant="warning" size="sm" onClick={statusChanger}>Submit</Button>
-        </Card>
-    )
+  return (
+    <Card style={{ width: "18rem" }} className="order-card">
+      <Card.Header>
+        <span>Date: {props.name}</span>
+      </Card.Header>
+      <ListGroup variant="flush">
+        <ListGroup.Item className="status">
+          <span>Status:</span>
+          <Dropdown>
+            <DropdownButton
+              id="dropdown-basic-button"
+              title={status}
+              onSelect={(e) => setStatus(e)}
+            >
+              <Dropdown.Item eventKey="accepted" style={{ color: "#5cb85c" }}>
+                Accept
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="rejected" style={{ color: "#d9534f" }}>
+                Reject
+              </Dropdown.Item>
+              <Dropdown.Item eventKey="pending" style={{ color: "#0275d8" }}>
+                Pending
+              </Dropdown.Item>
+            </DropdownButton>
+          </Dropdown>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <span>Price: {props.total_price} $</span>
+        </ListGroup.Item>
+      </ListGroup>
+      <Button variant="warning" size="sm" onClick={statusChanger}>
+        Submit
+      </Button>
+      <ListGroup.Item style={{ display: statusDisplay }}>
+        <span>Status changed to {props.status}</span>
+      </ListGroup.Item>
+    </Card>
+  );
 };
 
-const mapStateToProps = state => ({ user: state.user.currentUser, trigger: state.trigger });
+const mapStateToProps = (state) => ({
+  user: state.user.currentUser,
+  trigger: state.trigger,
+});
 
-const mapDispatchToProps = dispatch => ({
-    setTrigger: () => dispatch(setTrigger()),
+const mapDispatchToProps = (dispatch) => ({
+  setTrigger: () => dispatch(setTrigger()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CurrentDayOrders);
