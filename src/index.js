253a9ee7d60ac42111f30ffddedf64b2a78bd7fb
Roman <vzdrizhni@gmail.com>
2021-05-24 15:36:55 +0500

    Basic setup for connecting fron end to server with actioncable

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter, HashRouter } from "react-router-dom";
-import {Provider} from 'react-redux';
-import {store, persistor} from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { ActionCableProvider } from "react-actioncable-provider";
+import { API_WS_ROOT } from "./constants/index";
 
 ReactDOM.render(
   <Provider store={store}>
-      <HashRouter>
+    <HashRouter>
+      <ActionCableProvider url={API_WS_ROOT}>
         <PersistGate persistor={persistor}>
           <App />
         </PersistGate>
-      </HashRouter>
+      </ActionCableProvider>
+    </HashRouter>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
